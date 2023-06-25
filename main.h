8d3707fac0fc81e13c8cbf56e17bdc3926f5af9d
nourister <nourusterjuma@gmail.com>
2023-06-25 11:46:57 -0700

    my_shell

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ char* get_input();
 void replace_variable(char* command, const char* variable, const char* value);
 void handle_variables(char* command, int last_status);
 void execute_commands(char* commands[MAX_COMMANDS][MAX_ARGS], int num_commands);
-void execute_built_in_command(const char* command);
+void execute_command(const char* command);
 bool is_built_in_command(const char* command);
 bool is_background_command(const char* command);
 int parse_commands(char* input, char* commands[MAX_COMMANDS][MAX_ARGS]);
--- simple_shell.c
+++ simple_shell.c
@@ -71,100 +71,106 @@ void handle_variables(char* command, int last_status)
 
 void execute_commands(char* commands[MAX_COMMANDS][MAX_ARGS], int num_commands)
 {
-	int i;
-	int last_status = 0;
+    int i;
+    int last_status = 0;
 
-	for (i = 0; i < num_commands; i++)
-	{
-		char* command = commands[i][0];
+    for (i = 0; i < num_commands; i++)
+    {
+        char* command = commands[i][0];
 
-		if (command == NULL || command[0] == '#')
-		{
-			continue;
-		}
+        if (command == NULL || command[0] == '#')
+        {
+            continue;
+        }
 
-		if (is_built_in_command(command))
-		{
-			if (strcmp(command, "cd") == 0)
-			{
-				if (commands[i][1] == NULL)
-				{
-					printf("Missing directory argument for cd command\n");
-				}
-				else
-				{
-					if (chdir(commands[i][1]) != 0)
-					{
-						printf("Failed to change directory\n");
-					}
-				}
-			}
-			else if (strcmp(command, "exit") == 0)
-			{
-				exit(0);
-			}
-		}
-		else
-		{
-			pid_t pid = fork();
-			
-			if (pid == 0)
-			{
-				handle_variables(command, last_status);
-				execvp(command, commands[i]);
-				printf("Failed to execute command: %s\n", command);
-				exit(1);
-			}
-			else if (pid > 0)
-			{
-				if (!is_background_command(command))
-				{
-					int status;
-					waitpid(pid, &status, 0);
-					last_status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-				}
-			}
-			else
-			{
-				printf("Failed to fork process\n");
-				exit(1);
-			}
-		}
-	}
+        if (is_built_in_command(command))
+        {
+            if (strcmp(command, "cd") == 0)
+            {
+                if (commands[i][1] == NULL)
+                {
+                    printf("Missing directory argument for cd command\n");
+                }
+                else
+                {
+                    if (chdir(commands[i][1]) != 0)
+                    {
+                        printf("Failed to change directory\n");
+                    }
+                }
+            }
+            else if (strcmp(command, "exit") == 0)
+            {
+                exit(0);
+            }
+        }
+        else
+        {
+            pid_t pid = fork();
+
+            if (pid == 0)
+            {
+                handle_variables(command, last_status);
+                execvp(command, commands[i]);
+                printf("Command not found: %s\n", command);
+                exit(1);
+            }
+            else if (pid > 0)
+            {
+                if (!is_background_command(command))
+                {
+                    int status;
+                    waitpid(pid, &status, 0);
+                    last_status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+                }
+            }
+            else
+            {
+                printf("Failed to fork process\n");
+                exit(1);
+            }
+        }
+    }
 }
 
 /**
  * main - Entry point to a program
  * @argc: The number of command-line arguments
- * An array of strings representing the
- * command-line arguments
- * The exit status of the program
+ * @argv: An array of strings representing the command-line arguments
+ * Return: The exit status of the program
  */
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     char* input;
     char* commands[MAX_COMMANDS][MAX_ARGS];
     int num_commands;
     int i;
     char line[100];
 
-    if (argc == 2) {
+    if (argc == 2)
+    {
         FILE* file = fopen(argv[1], "r");
-        if (file == NULL) {
+        if (file == NULL)
+        {
             printf("Failed to open file: %s\n", argv[1]);
             return 1;
         }
 
-        while (fgets(line, sizeof(line), file)) {
+        while (fgets(line, sizeof(line), file))
+        {
             line[strcspn(line, "\n")] = '\0';
 
             num_commands = parse_commands(line, commands);
             execute_commands(commands, num_commands);
 
-            for (i = 0; i < num_commands; i++) {
+            for (i = 0; i < num_commands; i++)
+            {
                 int j;
-                for (j = 0; j < MAX_ARGS; j++) {
+                for (j = 0; commands[i][j] != NULL; j++)
+                {
                     free(commands[i][j]);
+                    commands[i][j] = NULL;
                 }
             }
         }
@@ -173,29 +179,28 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    while (1) {
+    while (1)
+    {
+        printf("$ ");
         input = get_input();
-
-        if (input == NULL) {
-            printf("\n");
+        if (input == NULL)
             break;
-        }
-
-        if (input[0] == '#') {
-            continue;
-        }
 
         num_commands = parse_commands(input, commands);
         execute_commands(commands, num_commands);
 
-        for (i = 0; i < num_commands; i++) {
+        for (i = 0; i < num_commands; i++)
+        {
             int j;
-            for (j = 0; j < MAX_ARGS; j++) {
+            for (j = 0; commands[i][j] != NULL; j++)
+            {
                 free(commands[i][j]);
+                commands[i][j] = NULL;
             }
         }
+
         free(input);
     }
+
     return 0;
 }
-
